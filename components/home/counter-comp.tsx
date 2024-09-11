6376639e11cd879334af664a498be83f92571d1e
Muhammad Shoaib Ali <muhammad.shoaib@rtechnologiespk.com>
2024-09-11 14:06:43 +0500

    Created Counter Component

--- components/home/counter-comp.tsx
+++ components/home/counter-comp.tsx
@@ -1,20 +1,75 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
+import CountUp from "react-countup";
+
+const milestonesList = [
+  {
+    metric: "Number of Projects Completed",
+    value: "69",
+    prefix: "+",
+  },
+  {
+    metric: "Happy Customers",
+    value: "25",
+    prefix: "+",
+  },
+  {
+    metric: "Increased Customer Satisfaction",
+    value: "97%",
+    prefix: "+",
+  },
+  {
+    metric: "Years of Experience",
+    value: "3",
+    prefix: "+",
+  },
+];
 
 export const CounterComp = () => {
   return (
-    <div>
-      <h1>
-        <motion.span
-          initial={{ opacity: 0, y: -10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-          className="text-h1"
-        >
-          This would be counter
-        </motion.span>
-      </h1>
+    <div
+      className="h-[330px] flex flex-col items-center mt-1"
+      style={{
+        background: "linear-gradient(to bottom, #6294BB 20%, #3B586E 90%)",
+      }}
+    >
+      <div className="mt-3">
+        <h1>
+          <motion.span
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="text-h1 text-white tracking-widest font-secondary"
+          >
+            MILESTONES
+          </motion.span>
+        </h1>
+      </div>
+      <div className="mt-16 flex flex-wrap justify-center gap-11">
+        {milestonesList.map((milestone, index) => (
+          <div
+            key={index}
+            className="text-white text-center flex flex-col items-center font-secondary"
+          >
+            <motion.div
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: index * 0.2 }}
+              className="text-4xl font-bold mb-2"
+            >
+              {milestone.prefix}{" "}
+              <CountUp
+                end={parseInt(milestone.value.replace("%", ""))}
+                duration={2}
+                separator=","
+              />
+              {milestone.value.includes("%") ? "%" : ""}
+            </motion.div>
+            <div className="text-lg">{milestone.metric}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- components/home/service-comp.tsx
+++ components/home/service-comp.tsx
@@ -4,17 +4,19 @@ import { motion } from "framer-motion";
 
 export const ServiceComp = () => {
   return (
-    <div>
-      <h1>
-        <motion.span
-          initial={{ opacity: 0, y: -10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-          className="text-h1"
-        >
-          This would be service
-        </motion.span>
-      </h1>
+    <div className="bg-[url('/assets/service-bg.svg')] bg-cover bg-center h-screen flex justify-center">
+      <div className="mt-3">
+        <h1>
+          <motion.span
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="text-h1 text-white font-secondary"
+          >
+            Services we offer for you
+          </motion.span>
+        </h1>
+      </div>
     </div>
   );
 };
